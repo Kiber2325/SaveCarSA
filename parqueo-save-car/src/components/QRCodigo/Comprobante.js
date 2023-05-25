3698d1569eadbf018c94000f92a94acdff764e0f
LionelCMZ <moraleslionel792@gmail.com>
2023-05-24 21:15:29 -0400

    CorreccionesComprobante

--- parqueo-save-car/src/components/QRCodigo/Comprobante.js
+++ parqueo-save-car/src/components/QRCodigo/Comprobante.js
@@ -26,13 +26,18 @@ const Comprobante = () => {
       return <h1 className='carga'>Cargando...</h1>;
     }
     //PDF
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
     const descargarPDF=()=>{
       const doc = new jsPDF();
       console.log(product)
       // Agrega contenido al PDF
-      let info=product.sitio+'\n'+
-      product.ciCliente+'\n'+product.placa+'\n'+product.celular+'\n'+product.monto+'\n'+product.fecha
-      +'\n'+product.hora;
+      let info='SaveCar\nFactura Nro:'+(getRandomInt(3)+1)+'\nPor la ocupación del sitio: '+product.sitio+'\nCI del cliente: '+
+      product.ciCliente+'\nPlaca del auto del cliente: '+product.placa+'\nCelular del auto del cliente:'+product.celular+
+      '\nMonto pagado: '+product.monto+' Bs.\nFecha del transacción: '+tiempoLimite()+
+      '\nHora de inicio reserva: '+product.hora+'\nHora límite de la reserva: '+formatoFecha()+'\n¡Atención! debes llegar antes a ocupar el sitio reservado antes de la hora de'+ 
+      '\nfinalización de la reserva, de lo contrario perderá su reserva.\n¡Gracias por la compra! Recuerda que debes enviar el comprobante\npara confirmar la reserva.';
       doc.text(info, 10, 10);
       //doc.addImage(getQRCodeDataUrl(), 'PNG', 10, 20, 50, 50);
   
@@ -77,7 +82,74 @@ const Comprobante = () => {
     }
     const tiempoLimite=()=>{
       let array=product.hora.split(':')
-      return (parseInt(product.hora.split(':')[0])+2)+':'+array[1]+':'+array[2];
+      let tiempoLimite=parseInt(product.hora.split(':')[0])
+      if(tiempoLimite===22){
+        tiempoLimite=0
+      }else if(tiempoLimite===23){
+        tiempoLimite=1
+      }else{
+        tiempoLimite=tiempoLimite+2
+      }
+      return formatoReloj(tiempoLimite)+':'+formatoReloj(parseInt(array[1]))+':'+formatoReloj(parseInt(array[2]));
+    }
+    const formatoReloj=(num)=>{
+      let res=num
+      if(res<10){
+        res='0'+res
+      }
+      return res+''
+    }
+    const formatoFecha=()=>{
+      let array=product.fecha.split(' ')
+      return traducirDia(array[0])+' '+traducirMes(array[1])+' '+array[2]+' '+array[3]
+    }
+    const traducirDia=(diaEn)=>{
+      let diaTraducido='';
+      if(diaEn==='Mon'){
+        diaTraducido='Lunes'
+      }else if(diaEn==='Thu'){
+        diaTraducido='Martes'
+      }else if(diaEn==='Wed'){
+        diaTraducido='Miércoles'
+      }else if(diaEn==='Tue'){
+        diaTraducido='Jueves'
+      }else if(diaEn==='Fri'){
+        diaTraducido='Viernes'
+      }else if(diaEn==='Sat'){
+        diaTraducido='Sábado'
+      }else if(diaEn==='Sun'){
+        diaTraducido='Domingo'
+      }
+      return diaTraducido;
+    }
+    const traducirMes=(mesEn)=>{
+      let mesTraducido='';
+      if(mesEn==='Jan'){
+        mesTraducido='Enero'
+      }else if(mesEn==='Feb'){
+        mesTraducido='Febrero'
+      }else if(mesEn==='Mar'){
+        mesTraducido='Marzo'
+      }else if(mesEn==='Apr'){
+        mesTraducido='Abril'
+      }else if(mesEn==='May'){
+        mesTraducido='Mayo'
+      }else if(mesEn==='Jun'){
+        mesTraducido='Junio'
+      }else if(mesEn==='Jul'){
+        mesTraducido='Julio'
+      }else if(mesEn==='Aug'){
+        mesTraducido='Agosto'
+      }else if(mesEn==='Sep'){
+        mesTraducido='Septiembre'
+      }else if(mesEn==='Oct'){
+        mesTraducido='Octubre'
+      }else if(mesEn==='Nov'){
+        mesTraducido='Noviembre'
+      }else if(mesEn==='Dec'){
+        mesTraducido='Diciembre'
+      }
+      return mesTraducido;
     }
     return (
       <div className='recibo'>
@@ -87,7 +159,7 @@ const Comprobante = () => {
           <p>Placa del auto del Cliente: {product.placa}</p>
           <p>Número celular del cliente: {product.celular}</p>
           <p>Monto pagado: {product.monto},0 Bs.</p>
-          <p>Fecha: {product.fecha}</p>
+          <p>Fecha: {formatoFecha()}</p>
           <p>Hora de inicio de la reserva: {product.hora}</p>
           <p>Hora de finalizacion de la reserva: {tiempoLimite()}</p>
           <p>¡Atención! debes llegar antes a ocupar el sitio reservado antes de la hora de finalización de la reserva, de lo contrario perderá su reserva.</p>
